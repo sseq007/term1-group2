f00d4c55ed86a6003407a0c21e79142753612be2
KodaHye <kodhdh09@gmail.com>
2023-01-22 23:17:30 +0900

    22.01.22 문제풀이 완료

--- DaHye/로봇이동거리.java
+++ DaHye/로봇이동거리.java
@@ -8,69 +8,81 @@ public class 로봇이동거리 {
 		// TODO Auto-generated method stub
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 		StringTokenizer st;
-
+		StringBuilder sb = new StringBuilder();
 		int T = Integer.parseInt(br.readLine());
-		int N = Integer.parseInt(br.readLine());
+
 
 		// 우, 하, 좌, 상
-		int[] dc = {1, 0, -1, 0};
-		int[] dr = {0, 1, 0, -1};
-//		for(int test_case = 1; test_case < T + 1; test_case++) {
-		char[][] arr = new char[N][N];
-		int count = 0;
-		// 배열 입력
-		for (int i = 0; i < arr.length; i++) {
-			st = new StringTokenizer(br.readLine());
-			for (int j = 0; j < arr[i].length; j++) {
-				arr[i][j] = st.nextToken().charAt(0);
+		int[] dc = { 1, 0, -1, 0 };
+		int[] dr = { 0, 1, 0, -1 };
+		for (int test_case = 1; test_case < T + 1; test_case++) {
+			int N = Integer.parseInt(br.readLine());
+			char[][] arr = new char[N][N];
+			int count = 0;
+			// 배열 입력
+			for (int i = 0; i < arr.length; i++) {
+				st = new StringTokenizer(br.readLine());
+				for (int j = 0; j < arr[i].length; j++) {
+					arr[i][j] = st.nextToken().charAt(0);
+				}
 			}
-		}
 
-		for (int i = 0; i < arr.length; i++) {
-			for (int j = 0; j < arr[i].length; j++) {
-				// 배열 범위 안에 있는지 확인하는 flag1
-				boolean flag = false;
-				int tmpI = i;
-				int tmpJ = j;
+			for (int i = 0; i < arr.length; i++) {
+				for (int j = 0; j < arr[i].length; j++) {
+					// 배열 범위 안에 있는지 확인하는 flag1
+					boolean flag = false;
+					int tmpI;
+					int tmpJ;
 
-				if (arr[tmpI][tmpJ] == 'A') {
-					while(true) {
-						tmpJ += 1;
-						tmpI += 0;
-						System.out.println("tmpI" + tmpI + " : " + "tmpJ" + tmpJ);
-						if(tmpJ < arr[i].length) flag = true;
-						if(flag == true && arr[tmpI][tmpJ] == 'S') {
-							count +=1;
+					if (arr[i][j] == 'A') {
+						tmpI = i;
+						tmpJ = j;
+						while (true) {
+							tmpJ += 1;
+							tmpI += 0;
+							flag = tmpJ < arr[i].length;
+							if (flag == true && arr[tmpI][tmpJ] == 'S') {
+								count += 1;
+							} else
+								break;
 						}
-						else continue;
-					}
-				} 
-//				else if (arr[i][j] == 'B') {
-//					for(int k = 0; k < 4; k++) {
-//						// 우
-//						if(k == 0) {
-//							tmpJ += dc[0];
-//							tmpI += dr[0];
-//							flag = tmpJ < arr[i].length;
-//							if(flag == false || arr[tmpI][tmpJ] != 'S') continue;
-//							else count += 1;
-//						} 
-//						// 좌
-//						else if(k == 2) {
-//							tmpJ += dc[2];
-//							tmpI += dc[2];
-//							
-//							flag = tmpJ < 0;
-//							if(flag == false || arr[tmpI][tmpJ] != 'S') continue;
-//							else count += 1;
-//						}
-//					}
-//				} else if(arr[i][j] == 'C') {
+					} else if (arr[i][j] == 'B') {
+						for (int k = 0; k < 4; k++) {
+							// 우, 좌
+							tmpI = i;
+							tmpJ = j;
+							if (k == 0 || k == 2) {
+								while (true) {
+									tmpJ += dc[k];
+									tmpI += dr[k];
+									flag = tmpJ < arr[i].length && tmpJ >= 0;
+									if (flag == true && arr[tmpI][tmpJ] == 'S')
+										count += 1;
+									else
+										break;
+								}
 
-//				}
-				System.out.println(count);
-//				System.out.println(tmpI + " " + tmpJ);
+							}
+						}
+					} else if (arr[i][j] == 'C') {
+						for (int k = 0; k < 4; k++) {
+							tmpI = i;
+							tmpJ = j;
+							while (true) {
+								tmpJ += dc[k];
+								tmpI += dr[k];
+								flag = tmpJ < arr[i].length && tmpJ >= 0 && tmpI < arr.length && tmpI >= 0;
+								if (flag == true && arr[tmpI][tmpJ] == 'S')
+									count += 1;
+								else
+									break;
+							}
+						}
+					}
+				}
 			}
+			sb.append("#" + test_case + " " + count + "\n");
 		}
+		System.out.println(sb);
 	}
 }
