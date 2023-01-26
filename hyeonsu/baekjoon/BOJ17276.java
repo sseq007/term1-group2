d562d9fdacb30a28cddfd3f0b319e14caecc24e3
김현수 <ekclstkfka5@gmail.com>
2023-01-26 17:55:49 +0900

    refactor: [BOJ17276] 문제 풀이 미완료
    구현은 했지만 답이 좀 다르게 나옴.

---
+++ hyeonsu/baekjoon/BOJ17276.java
@@ -0,0 +1,96 @@
+package baekjoon;
+
+import java.io.*;
+import java.util.Arrays;
+import java.util.StringTokenizer;
+
+public class BOJ17276 {
+    static BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+    static BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+    static StringTokenizer st;
+    static int t;
+    public static void main(String[] args) throws IOException {
+        t = stoi(br.readLine());
+
+        while (t-- > 0) {
+            //배열 초기화
+            st = new StringTokenizer(br.readLine());
+            int n = stoi(st.nextToken());
+            int d = stoi(st.nextToken());
+            boolean turnRight = d > 0;
+            d = Math.abs(d) / 45;
+            int[][] arr = new int[n][n];
+
+            for (int i = 0; i < n; i++) {
+                st = new StringTokenizer(br.readLine());
+                for (int j = 0; j < n; j++) {
+                    arr[i][j] = stoi(st.nextToken());
+                }
+            }
+
+            //로직
+            while (d-- > 0) {
+                arr = turnRight ? rotateRight(arr, n) : rotateLeft(arr, n);
+            }
+
+            //배열 입력
+            for (int i = 0; i < n; i++) {
+                for (int j = 0; j < n; j++) {
+                    bw.write(arr[i][j] + " ");
+                }
+                bw.write("\n");
+            }
+
+        }
+
+        //정답 출력
+        bw.flush();
+        bw.close();
+    }
+
+    static int[][] rotateRight(int[][] arr, int n) {
+        int[][] tmp = arr.clone();
+
+        //첫 대각선
+        for (int j = 0; j < n; j++) {
+            tmp[j][n / 2] = arr[j][j];
+        }
+        //가운데 수직
+        for (int j = 0; j < n; j++) {
+            tmp[j][n-j-1] = arr[j][n / 2];
+        }
+        //부대각선
+        for (int j = 0; j < n; j++) {
+            tmp[n / 2][j] = arr[n-j-1][j];
+        }
+        //수평선
+        for (int j = 0; j < n; j++) {
+            tmp[j][j] = arr[n / 2][j];
+        }
+        return tmp;
+    }
+
+    static int[][] rotateLeft(int[][] arr, int n) {
+        int[][] tmp = arr.clone();
+
+        //첫 대각선 -> 수평선
+        for (int j = 0; j < n; j++) {
+            tmp[n / 2][j] = arr[j][j];
+        }
+        //수평선 -> 부대각선
+        for (int j = 0; j < n; j++) {
+            tmp[n-j-1][j] = arr[n / 2][j];
+        }
+        //부대각선 -> 수직선
+        for (int j = 0; j < n; j++) {
+            tmp[n-j-1][n / 2] = arr[n-j-1][j];
+        }
+        //수직선 -> 첫 대각선
+        for (int j = 0; j < n; j++) {
+            tmp[n-j-1][n-j-1] = arr[n-j-1][n / 2];
+        }
+        return tmp;
+    }
+
+    static int stoi(String s) {return Integer.parseInt(s);}
+}
