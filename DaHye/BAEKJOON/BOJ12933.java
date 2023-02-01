bdfdfc6c85f3a3f1bfd349ffa4b99f71cf3d7366
KodaHye <kodhdh09@gmail.com>
2023-02-01 20:21:02 +0900

    fix: 오리 문제 오류 수정

--- DaHye/BAEKJOON/BOJ12933.java
+++ DaHye/BAEKJOON/BOJ12933.java
@@ -10,40 +10,35 @@ public class BOJ12933 {
 	public static void main(String[] args) throws Exception {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 		StringBuilder input = new StringBuilder(br.readLine());
-
+		
+		int duckminCount = 0;
 		int duckCount = 0;
 		int minqIdx = 0;
 		int minkIdx = 0;
 		
-		for (int i = 0; i < input.length(); i++) {
-			if(input.charAt(i) == 'q') {
-				minqIdx = i;
-				break;
-			}
-		}
-		
-		// 시작 단어가 q가 될 수 있도록
-		input = new StringBuilder(input.toString().substring(minqIdx, input.length()));
-		
-		for(int i = 0; i < input.length(); i++) {
-			if(input.charAt(i) == 'k') {
-				minkIdx = i;
-				break;
-			}
-		}
-		
-		for(int i = minqIdx; i < minkIdx; i++) {
-			if(input.charAt(i) == 'q') duckCount++;
-		}
-		
-		int duckCountTmp = 0;
 		while (true) {
+			// 최소 오리 개수를 세기 위한 코드
+			// input: quackqquuaacckk일 경우 생각해보기
+			int duckmintmpCount = 0;
+			for (int i = 0; i < input.length(); i++) {
+				if(input.charAt(i) == 'q') {
+					minqIdx = i;
+					break;
+				}
+			}
 
+			for(int i = minqIdx; i < input.indexOf("k"); i++) {
+				if(input.charAt(i) == 'q') duckmintmpCount++;
+			}
+			
+			duckminCount = Math.max(duckmintmpCount, duckminCount);
+			
 			int qIdx = -1;
 			int uIdx = -1;
 			int aIdx = -1;
 			int cIdx = -1;
 			int kIdx = -1;
+			boolean flag = false;
 			for (int i = 0; i < input.length(); i++) {
 				if (input.toString().charAt(i) == 'q') {
 					qIdx = i;
@@ -74,23 +69,26 @@ public class BOJ12933 {
 					break;
 				}
 			}
-
-			if(qIdx != -1 && uIdx != -1 && aIdx != -1 && cIdx != -1 && kIdx != -1) {
-//				System.out.println(qIdx + " " + uIdx + " " + aIdx + " " + cIdx + " " +kIdx);
-				duckCountTmp++;
+			
+			if(qIdx < uIdx && uIdx < aIdx && aIdx < cIdx && cIdx < kIdx) {
+				flag = true;
+			}
+			
+			if(qIdx != -1 && uIdx != -1 && aIdx != -1 && cIdx != -1 && kIdx != -1 && flag == true) {
 				input.deleteCharAt(kIdx);
 				input.deleteCharAt(cIdx);
 				input.deleteCharAt(aIdx);
 				input.deleteCharAt(uIdx);
 				input.deleteCharAt(qIdx);
-
 			} else break;
 		}
 		
-		duckCount = Math.min(duckCount, duckCountTmp);
-		if(duckCount == 0) System.out.println(-1);
-		else System.out.println(duckCount);
-//		System.out.println(Math.min(duckCount, duckCountTmp));
+		// duckminCount == 0: 엔터만 누른 경우
+		if(input.toString().equals("") && duckminCount != 0) {
+			System.out.println(duckminCount);
+		} else {
+			System.out.println(-1);
+		}
 	}
 
 }
