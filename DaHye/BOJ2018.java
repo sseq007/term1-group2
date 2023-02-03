3255406c77d2ecbd5696a07742df2396aa7bb7d4
KodaHye <kodhdh09@gmail.com>
2023-02-03 23:21:18 +0900

    add: '수들의 합' 문제풀이 완료

---
+++ DaHye/BOJ2018.java
@@ -0,0 +1,39 @@
+import java.util.Scanner;
+
+public class BOJ2018 {
+	/*
+	수들의 합
+	어떠한 자연수 N의 연속된 자연수의 합의 개수
+	투 포인터 이용
+	 */
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		int count = 1;
+		int startIdx = 0;
+		int endIdx = 0;
+		
+		int N = sc.nextInt();
+		int[] arr = new int[N];
+		
+		for (int i = 0; i < arr.length; i++) {
+			arr[i] = i + 1;
+		}
+		
+		int sum = arr[0];
+		while(endIdx < N - 1) {
+			if(sum < N) {
+				endIdx++;
+				sum += arr[endIdx];
+			} else if(sum > N) {
+				sum -=arr[startIdx];
+				startIdx++;
+			} else if(sum == N) {
+				endIdx++;
+				sum += arr[endIdx];
+				count++;
+			}
+		}
+		System.out.println(count);
+	}
+
+}
