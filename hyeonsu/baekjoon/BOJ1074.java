37feff52f88a4f7694e5f4b264a25fb3fc886b9f
김현수 <ekclstkfka5@gmail.com>
2023-02-07 11:03:34 +0900

    feat: [BOJ1074] Z 풀이 완료
    BOJ1074 Z 풀이를 완료함.

--- hyeonsu/baekjoon/BOJ1074.java
+++ hyeonsu/baekjoon/BOJ1074.java
@@ -5,8 +5,8 @@ import java.util.StringTokenizer;
 
 public class BOJ1074 {
 
-    static int[] dx = { 0, 1, 0};
-    static int[] dy = { 1, -1, 1};
+    static int[] dx = {0, 0, 1, 0};
+    static int[] dy = {0, 1, -1, 1};
 
     static BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
     static StringTokenizer st;
@@ -21,15 +21,45 @@ public class BOJ1074 {
         len = (int) Math.pow(2, n);
 
         //로직
-        recur(0, 0, 0, 0);
+        recur(len, 0, 0);
 
         System.out.println(ans);
     }
 
     static int stoi(String s) {return Integer.parseInt(s);}
 
-    static void recur(int len, int idx, int cnt) {
+    static void recur(int len, int x, int y) {
+        if (len == 2) {
+            for (int i = 0; i < 4; i++) {
+                x += dx[i];
+                y += dy[i];
 
-        recur();
+                if (x == r && y == c) {
+                    ans = cnt;
+                    return;
+                }
+                cnt++;
+            }
+            return;
+        }
+
+        int nextLen = len / 2;
+        int sumCnt = (int) Math.pow((double)len / 2, 2);
+
+        if (x + nextLen - 1 >= r && y + nextLen - 1 >= c) {
+            recur(nextLen, x, y);
+        }
+        else if (x + nextLen - 1 >= r) {
+            cnt += sumCnt;
+            recur(nextLen, x, y + nextLen);
+        }
+        else if (y + nextLen - 1 >= c) {
+            cnt += sumCnt * 2;
+            recur(nextLen, x + nextLen, y);
+        }
+        else {
+            cnt += sumCnt * 3;
+            recur(nextLen, x + nextLen, y + nextLen);
+        }
     }
 }
