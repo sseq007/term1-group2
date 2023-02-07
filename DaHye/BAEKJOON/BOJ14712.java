cd2068b523f51c6d7462645ea9db1807fd85cc86
KodaHye <kodhdh09@gmail.com>
2023-02-08 00:15:11 +0900

    add: "넴모넴모" 문제풀이 완료

--- DaHye/BAEKJOON/BOJ14712.java
+++ DaHye/BAEKJOON/BOJ14712.java
@@ -5,44 +5,48 @@ import java.util.HashSet;
 import java.util.Scanner;
 
 public class BOJ14712 {
-	
+
 	static int[] arr;
 	static int[] square;
-	static boolean[] v;
-	static HashSet<boolean[]> set = new HashSet<>();
 	static int count = 0;
 	static int N;
 	static int M;
-	
+
 	public static void main(String[] args) {
 		Scanner sc = new Scanner(System.in);
 		N = sc.nextInt();
 		M = sc.nextInt();
-		
+
 		arr = new int[N * M];
-		square = new int[] {0, 1};
-		v = new boolean[N * M];
-		
+		square = new int[] { 0, 1 };
+
 		func(0);
 		System.out.println(count);
 	}
-	static int m = - 1;
-	public static void func(int k){
+
+	static int m = -1;
+
+	public static void func(int k) {
+
 		boolean flag = false;
-		for(int i = 0; i < M * N - N - 1; i++) {
-			if((v[i] == true) && (v[i + 1] == true) && (v[i + N] == true) && (v[i + N + 1] == true)) {
-				flag = true;
+
+		if (k == arr.length) {
+			for (int i = 0; i < M * N - N - 1; i++) {
+				if((i + 1) % N == 0) {
+					continue;
+				}
+				if ((arr[i] == 1) && (arr[i + 1] == 1) && (arr[i + N] == 1) && (arr[i + N + 1] == 1)) {
+					flag = true;
+					break;
+				}
 			}
+			if (flag == false) count++;
+
+			return;
 		}
-		if(flag == false) count++;
-		
-		for(int i = 0; i < v.length; i++) {
-			if(m < i && !v[i]) {
-				v[i] = true;
-				func(k + 1);
-				v[i] = false;
-				m = i;
-			}
+		for (int i = 0; i < square.length; i++) {
+			arr[k] = i;
+			func(k + 1);
 		}
 	}
 }
