6daf87217e6b0646f1b3bb13609d1dfbaac70aed
고다혜 <kdhdh09@naver.com>
2023-02-10 17:52:58 +0900

    add: program solving

--- DaHye/BAEKJOON/BOJ2164.java
+++ DaHye/BAEKJOON/BOJ2164.java
@@ -4,25 +4,23 @@ import java.util.LinkedList;
 import java.util.Queue;
 import java.util.Scanner;
 
-public class BOJ2164 {
-
+public class BOJ2164 {	
+	/* 
+	 * 카드2
+	 */
+	static Queue<Integer> queue = new LinkedList<>();
 	public static void main(String[] args) {
 		Scanner sc = new Scanner(System.in);
 		
 		int N = sc.nextInt();
-		Queue <Integer> cards = new LinkedList<>();
 		
-		for(int i = 1; i < N + 1; i++) {
-			cards.add(i);
-		}
+		for(int i = 1; i < N + 1; i++) queue.add(i);
 		
-		while(cards.size() > 1) {
-			cards.remove();
-			cards.add(cards.peek());
-			cards.remove();
-			
+		while(queue.size() > 1) {
+			queue.poll();
+			queue.add(queue.peek());
+			queue.poll();
 		}
-		System.out.println(cards.peek());
+		System.out.println(queue.peek());
 	}
-
 }
