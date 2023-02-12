df8aa07b66112ec0098f449e65090b640ecf75ad
KodaHye <kodhdh09@gmail.com>
2023-02-12 23:12:57 +0900

    add: 'z' 문제풀이 완료

--- DaHye/BAEKJOON/BOJ1074.java
+++ DaHye/BAEKJOON/BOJ1074.java
@@ -4,10 +4,7 @@ import java.io.BufferedReader;
 import java.io.InputStreamReader;
 import java.util.Arrays;
 import java.util.StringTokenizer;
-class Point {
-	int x;
-	int y;
-}
+
 public class BOJ1074 {
 	/*
 	 * Z
