a13b59da70f39c4731e97a5f8f49bc4f40c57351
ki_mensoo <ekclstkfka5@gmail.com>
2023-02-18 00:18:25 +0900

    feat: [BOJ1244] 스위치 켜고 끄기 풀이 완료
    BOJ1244 스위치 켜고끄기를 완료함.

--- hyeonsu/baekjoon/BOJ1244.java
+++ hyeonsu/baekjoon/BOJ1244.java
@@ -1,10 +1,7 @@
 package baekjoon;
 
-import java.io.BufferedReader;
-import java.io.IOException;
-import java.io.InputStreamReader;
-import java.util.Arrays;
-import java.util.StringTokenizer;
+import java.io.*;
+import java.util.*;
 
 public class BOJ1244 {
 
@@ -31,11 +28,10 @@ public class BOJ1244 {
             int num = stoi(st.nextToken()) - 1;
 
             if (gender == 1) {
-                //자기가 받은 수의 배수가 켜져있으면 끈다.
+                //자기가 받은 수의 배수의 상태를 바꾼다.
                 for (int j = num; j < n; j += num + 1) {
-                    if (!switches[j]) switches[j] = !switches[j];
+                    switches[j] = !switches[j];
                 }
-                System.out.println(Arrays.toString(switches));
             } else {
                 /*
                 자기가 받은 수와 같은 번호가 붙은 스위치를 중심으로 좌우가 대칭이면서 가장 많은 스위치를 포함하는 구간을 찾아서
@@ -43,20 +39,25 @@ public class BOJ1244 {
                  */
                 int l = num - 1;
                 int r = num + 1;
+                switches[num] = !switches[num];
                 if (l < 0 || r >= n) continue;
 
                 while (true) {
                     if (l < 0 || r >= n) break;
                     if (switches[l] != switches[r]) break;
-                    switches[l] = !switches[l++];
+                    switches[l] = !switches[l--];
                     switches[r] = !switches[r++];
                 }
-                System.out.println(Arrays.toString(switches));
             }
         }
 
-        for (int i = 0; i < n; i++) {
-            sb.append(switches[i] ? 1 : 0).append(" ");
+        //정답 출력
+        int idx = 0;
+
+        while (true) {
+            if (idx == switches.length) break;
+            sb.append(switches[idx++]? "1 " : "0 ");
+            if (idx % 20 == 0) sb.append("\n");
         }
 
         System.out.println(sb);
