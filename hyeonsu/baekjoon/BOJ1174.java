795d1e148aa7ba74ca0d47a8f32babdb1665ae45
ki_mensoo <ekclstkfka5@gmail.com>
2023-02-22 20:52:08 +0900

    feat: [BOJ1174] 감소하는 수 풀이 완료
    BOJ1174 감소하는 수 풀이를 완료함.

--- hyeonsu/baekjoon/BOJ1174.java
+++ hyeonsu/baekjoon/BOJ1174.java
@@ -3,35 +3,30 @@ package baekjoon;
 import java.util.*;
 
 public class BOJ1174 {
-    static final int N = 1000000;
+    static int[] nums = {9, 8, 7, 6, 5, 4, 3, 2, 1, 0};
 
-    static List<Integer> list = new ArrayList<>();
+    static List<Long> list = new ArrayList<>();
 
     public static void main(String[] args) {
         int n = new Scanner(System.in).nextInt();
-        long i = 0;
-        while (true) {
-            if (list.size() == n) break;
-            String num = String.valueOf(i);
-            solve(num, 1, num.length());
-            i++;
+        if (n >= 1023) System.out.println(-1);
+        else {
+            solve(0, 0, 10, "");
+            Collections.sort(list);
+            System.out.println(list.get(n));
         }
-        Collections.sort(list);
-        System.out.println("=====" + list.size() + "=====");
-        for (int a : list) {
-            System.out.println(a);
-        }
-        System.out.println(n > list.size() || n == 0 ? -1 : list.get(n - 1));
     }
 
-    private static void solve(String num, int k, int len) {
-        if (k == len) {
-            list.add(Integer.parseInt(num));
+    private static void solve(int k, int depth, int len, String num) {
+        if (depth == len) {
+            if (num.equals("")) return;
+            list.add(Long.parseLong(num));
             return;
         }
 
-        if (num.charAt(k - 1) - '0' <= num.charAt(k) - '0') return;
+        if (k > len) return;
 
-        solve(num, k + 1, len);
+        solve(k + 1, depth + 1, len, num);
+        solve(k + 1, depth + 1, len, num + nums[k]);
     }
 }
