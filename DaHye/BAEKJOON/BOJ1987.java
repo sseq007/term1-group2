3538ff9b8e78da20b7158a50fa9dde09bbb8376a
KodaHye <kodhdh09@gmail.com>
2023-02-24 09:39:17 +0900

    refact: '알파벳' 불필요한 코드 수정

--- DaHye/BAEKJOON/BOJ1987.java
+++ DaHye/BAEKJOON/BOJ1987.java
@@ -12,8 +12,7 @@ public class BOJ1987 {
 	static StringTokenizer st;
 	static int R, C, max = 1;
 	static char[][] map;
-	static boolean[] v; // [0]: 해당 문자가 사용 되었는지, [1]: 해당 알파벳이 나왔는지
-	static boolean[][] mapV;
+	static boolean[] v; 
 	static int count = 1; // 지나간 칸 수 이므로 초기값: 1
 	
 	public static void main(String[] args) throws Exception {
@@ -22,7 +21,6 @@ public class BOJ1987 {
 		C = Integer.parseInt(st.nextToken());
 		map = new char[R][C];
 		v = new boolean[26];
-		mapV = new boolean[R][C];
 		
 		for(int i = 0; i < map.length; i++) {
 			String input = br.readLine();
@@ -30,9 +28,6 @@ public class BOJ1987 {
 				map[i][j] = input.charAt(j);
 			}
 		}
-		
-		v[map[0][0] - 'A'] = true; // 해당 알파벳 나왔다고 표시
-		mapV[0][0] = true;
 		
 		dfs(0, 0);
 		
@@ -42,19 +37,20 @@ public class BOJ1987 {
 	static int[] dr = {1, -1, 0, 0};
 	static int[] dc = {0, 0, 1, -1};
 	private static void dfs(int r, int c) {
-
+		
+		v[map[0][0] - 'A'] = true; // 해당 알파벳 나왔다고 표시
+		
 		for(int d = 0; d < 4; d++) {
 			int nr = r + dr[d];
 			int nc = c + dc[d];
 			
 			if(!check(nr, nc)) continue;
-			if(!v[map[nr][nc] - 'A'] && !mapV[nr][nc]) {
+			if(!v[map[nr][nc] - 'A']) {
 				v[map[nr][nc] - 'A'] = true;
 				count++;
 				max = Math.max(count, max);
 				dfs(nr, nc);
 				count--;
-				mapV[nr][nc] = false;
 				v[map[nr][nc] - 'A'] = false;
 			}
 		}
